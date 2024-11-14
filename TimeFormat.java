d452b241e9c90fa47824b4a5784295b7babaf9ac
Amit352352 <amit.peled01@post.runi.ac.il>
2024-11-14 23:40:34 +0200

    finishedFInale

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,15 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+        if (hours<12) { if (minutes>9) System.out.println(hours+":"+minutes+" AM"); 
+		else System.out.println(hours+":0"+minutes+" AM");
+		}
+		else if (hours==12) {if (minutes>9) System.out.println(hours+":"+minutes+" PM"); 
+		else System.out.println(hours+":0"+minutes+" PM");
+		}
+		else {int NewHours = hours-12; //if hours>12
+			if (minutes>9) System.out.println(NewHours+":"+minutes+" PM"); 
+		else System.out.println(NewHours+":0"+minutes+" PM");
+		}
 	}
 }
