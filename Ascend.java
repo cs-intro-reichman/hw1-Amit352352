a782d3a1409bf8185f475432ed5fe3f447f08725
Amit352352 <amit.peled01@post.runi.ac.il>
2024-11-14 16:01:59 +0200

    finished

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,13 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (lim* Math.random());
+		int b = (int) (lim* Math.random());
+		int c = (int) (lim* Math.random());
+		int min = Math.min(c, Math.min(a,b));
+		int max = Math.max(c, Math.max(a,b));
+		int mid = a + b + c - min -max;a
+		System.out.println(a+" "+b+" "+c+" min is "+min);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,15 @@
-// Computes the future value of a saving investment.
+
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int curretValue = Integer.parseInt(args[0])  ;
+		int annualinterest = Integer.parseInt(args[1]);
+		int yearn =Integer.parseInt(args[2]);
+		double rate = (1+ (double) annualinterest/100);
+		double x = Math.pow(rate, (double) yearn);
+		double futurevalue = (curretValue*x); 
+
+		System.out.println("After "+yearn+" years, $"+curretValue+" saved at "+annualinterest+"% will yield $"+(int) futurevalue);
+
+
 	}
 }
