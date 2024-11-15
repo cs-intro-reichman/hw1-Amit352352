7898dfc387786b73998ea6fec61d5acb3d50b53e
Amit352352 <amit.peled01@post.runi.ac.il>
2024-11-15 21:30:03 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int curretValue = Integer.parseInt(args[0])  ;
 		double annualinterest = Double.parseDouble(args[1]);
 		int yearn =Integer.parseInt(args[2]);
-		int annualinterest2 = (int) annualinterest ;
-		double rate = (1+ (double) annualinterest2/100);
+		//int annualinterest2 = (int) annualinterest ;
+		double rate = (1+ (double) annualinterest/100);
 		double x = Math.pow(rate, (double) yearn);
 		double futurevalue  =  (curretValue*x); 
 
